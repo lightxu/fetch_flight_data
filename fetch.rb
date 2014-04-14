f14b75028beef01f4390f86a8e278f6f752a7593
Shicheng Xu <me@lightxu.com>
2014-04-14 23:33:06 +0800

    Initial commit.

---
+++ fetch.rb
@@ -0,0 +1,69 @@
+require 'watir'
+require 'timeout'
+
+File.open("flight_price.csv", "w") do |csv_file|
+  csv_file.puts "Timestamp, price, plane1, plane2, plane3"
+end
+
+while (1)
+  print "Fetch start"
+  browser = Watir::Browser.new
+
+  # Tianxun
+  #　browser.goto "http://www.tianxun.cn/transport/flights/pvg/pit/140805/airfares-from-shanghai-pu-dong-to-pittsburgh-int-l-apt.-in-august-2014.html"
+
+
+  # 全日空
+  browser.goto "http://www.ana.co.jp/asw/wws/cn/c/"
+
+  form = browser.form :name, "segConditionForm"
+  single = browser.radio :id, "ticket_2"
+  from = browser.text_field :name, "DepApoText"
+  to = browser.text_field :name, "ArrApoText"
+
+  single.click
+  from.set "上海(浦东)/Shanghai(Pudong)[PVG]"
+  to.set "匹兹堡(PIT)/Pittsburgh[PIT]"
+
+  browser.link(:id, "wayTo").click
+  browser.link(:id, "calendar-next").click
+  browser.link(:id, "0805").click
+
+  form.submit
+
+  if not browser.table(:xpath, "//*[@id=\"bodyArea\"]/table[15]").exists?
+    puts "Please input verification code in the webpage XD.\n Then press enter in this console."
+    gets
+  end
+  File.open("flight_price.csv", "a") do |csv_file|
+    for i in 0...5
+      row = "#{Time.now}, " +
+        browser.span(:xpath, "//*[@id=\"bodyArea\"]/table[#{15+i}]/tbody/tr[1]/td/table/tbody/tr/td[4]/span[1]").text.gsub(/,|元|～/, "") +
+        ", " +
+        browser.td(:xpath, "//*[@id=\"bodyArea\"]/table[#{15+i}]/tbody/tr[3]/td[2]").text +
+        ", " +
+        browser.td(:xpath, "//*[@id=\"bodyArea\"]/table[#{15+i}]/tbody/tr[4]/td[2]").text +
+        ", " +
+        browser.td(:xpath, "//*[@id=\"bodyArea\"]/table[#{15+i}]/tbody/tr[5]/td[2]").text
+      puts row
+      csv_file.puts row
+    end
+  end
+
+  browser.close
+  
+  # http://stackoverflow.com/questions/22796591/ruby-gets-wait-2-seconds-and-then-set-the-value
+  begin
+    puts "Press enter if you want to pause, I will wait for 5 seconds..."
+    wait = Timeout::timeout(5) do
+      gets
+    end
+    puts "Aye aye captain, please press enter again to continue."
+    gets
+    puts "I will start working immediately!"
+  rescue Timeout::Error
+    puts "Time out! Let's wait for 30 minutes to fetch next flight data"
+    sleep 30*60
+    # sleep 1
+  end
+ end
---
+++ install_watir.bat
@@ -0,0 +1,7 @@
+# http://ruby.taobao.org/
+
+gem sources --remove https://rubygems.org/
+gem sources -a http://ruby.taobao.org/
+gem sources -l
+
+gem install watir
